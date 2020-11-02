40100ae546808efbdd493dbafcf8fce36bb8d0d1
hieunugent <hieunugent@gmail.com>
2020-11-01 21:53:12 -0800

    add this

--- problem3.py
+++ problem3.py
@@ -1,32 +1,23 @@
 def isPrime(n):
     if n <= 1:
-        return False
-    
+        return False 
     for i in range(2, n):
         if (n % i == 0):
             return False
     return True
-    
-if(isPrime(9)):
-    print('prime numbeer')
-else:
-    print('not prime number')
-
-def isInt(num):
-    if(isinstance(num, int)):
-        print("True")
-        return True
-    return False
-
-for i in range(2, 10):
-    if(isPrime(i)& isInt(10/i)):
-        print(i)
-    else:
-        print("")
-
-isInt(4/4)
 
 
-        
+
+def fun(num):
+    if (num > 1):
+        for i in range(2, num ):
+            if (num % i)==0:
+                 fun(num//i)
+            print(i)
+            
+                
+fun(45)
+
+a = 3*3*5*5       
 
 
