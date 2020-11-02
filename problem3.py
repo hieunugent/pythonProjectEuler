e865a4110c6d22ef2b6cc04af848112e52281049
hieunugent <hieunugent@gmail.com>
2020-11-02 01:25:52 -0800

    add

--- problem3.py
+++ problem3.py
@@ -3,21 +3,34 @@ def isPrime(n):
         return False 
     for i in range(2, n):
         if (n % i == 0):
-            return False
-    return True
+            return 0
+    return 1
 
-
+isPrime(4)
 
 def fun(num):
-    if (num > 1):
-        for i in range(2, num ):
-            if (num % i)==0:
-                 fun(num//i)
-            print(i)
-            
-                
-fun(45)
+    i = 2
+    while i*i <= num:
+        if num % i:
+            i+=1
+        else:
+            num//=i
+    return num
+   
 
-a = 3*3*5*5       
+print(fun(600851475143))
 
+def primeFactor(num):
+    i = 2
+    factor=[]
+    while i*i <=num:
+        if num %i:
+            i+=1
+        else:
+            num//=i
+            factor.append(i)
+    if num > 1:
+        factor.append(num)
+    return factor
 
+print(primeFactor(600851475143))
