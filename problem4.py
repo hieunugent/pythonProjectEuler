baf13285f5537dab675b5c7e8e6083ec6c95e880
hieunugent <hieunugent@gmail.com>
2020-11-02 12:14:59 -0800

    add update

--- problem4.py
+++ problem4.py
@@ -1,4 +1,4 @@
-def palindrome(n):
+def isPalindrome(n):
     temp=n
     rev=0
     while(n > 0):
@@ -10,4 +10,22 @@ def palindrome(n):
     else:
         return False
 
-print(palindrome(10011))
+
+def fun(a, b):
+    temp = 0
+    for i in range(999,1):
+        for j in range(999,1):
+            if palindrome(i*j):
+                temp = max(temp, j*j)
+    return temp
+
+print(fun(999,999))
+
+start = 999*999
+while (not isPalindrome(start)):
+    start-=1
+
+print(start)
+
+
+
