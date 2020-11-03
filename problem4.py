fa536f8afcec24977ea730b85b4f2cf12644de25
hieunugent <hieunugent@gmail.com>
2020-11-02 22:03:29 -0800

    add problem 4

--- problem4.py
+++ problem4.py
@@ -11,21 +11,26 @@ def isPalindrome(n):
         return False
 
 
-def fun(a, b):
-    temp = 0
-    for i in range(999,1):
-        for j in range(999,1):
-            if palindrome(i*j):
-                temp = max(temp, j*j)
-    return temp
+# find the possible max of palindrome
 
-print(fun(999,999))
 
-start = 999*999
-while (not isPalindrome(start)):
-    start-=1
-
-print(start)
+# consider if exits a factor of the num ber
 
 
 
+def brute_force(n):
+    largest_number = (10 ** n) - 1
+    smallest_number = (10 ** (n-1))
+    largest_palindrome = 0
+    for outer_i in range (largest_number, smallest_number,-1):
+        for inner_i in range (outer_i, smallest_number, -1):
+            number = outer_i * inner_i
+            if(isPalindrome(number) & (number > largest_palindrome)):
+                largest_palindrome = number
+                if(inner_i > smallest_number):
+                    smallest_number = inner_i
+                break
+    return largest_palindrome
+
+
+print(brute_force(3))
