82d74a560fe2453c42d764ae47a75b6cd541ed7e
hieunugent <hieunugent@gmail.com>
2020-11-26 01:27:33 -0800

    add challeneger day6

--- day6.py
+++ day6.py
@@ -5,12 +5,33 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chooseword = random.choice(word_list)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("guess a letter and assign their answer to a variable called guess. Make guess lowercase.").lower()
+
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chooseword:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+display=[]
+word_length = len(chooseword)
+for _ in range(word_length):
+    display += "_"
+def game(guess):
+    count = 0
+    for pos in range(word_length):
+        letter = chooseword[pos]
+        if letter == guess:
+            display[pos] = letter
+          
+  
+
+gameover= False
+while(not gameover):
+    guess = input(
+        "guess a letter and assign their answer to a variable called guess. Make guess lowercase.").lower()
+    game(guess)
+    print(display)
+    if "_" not in display:
+        print("You are WIn")
+        gameover=True
+
+
+
+
 
 
