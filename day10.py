b00aa6659d1f347aee4f98b68044933444145846
hieunugent <hieunugent@gmail.com>
2020-11-28 00:27:32 -0800

    final day10

--- day10.py
+++ day10.py
@@ -15,13 +15,24 @@ operations = {
     "/":devide,
     "*": multiply,
 }
-
-num1 =int(input("please enter the first number: "))
-num2 = int(input("please enter the second number: "))
-for char in operations:
-    print(char)
-operations_symbol= input("please enter the symbol below: ")
-calculator_function = operations[operations_symbol]
-firstAnswer = calculator_function(num1,num2)
-print(f"{num1} {operations_symbol} {num2} = {firstAnswer}")
-
+def calculator():
+    num1 = float(input("please enter the first number: "))
+    for char in operations:
+        print(char)
+    exit = False
+    while not exit:
+        operations_symbol = input("Pick an Operation: ")
+        num2 = float(input("what is your next number? "))
+        calculator_function = operations[operations_symbol]
+        result = calculator_function(num1,num2)
+        print(f"{num1} {operations_symbol} {num2} = {result} ")
+        wantcontinue = input(f"Type 'y' if you want to continue with {result} type 'rs' to reset or 'exit' to exit ").lower()
+        if(wantcontinue =="y" or wantcontinue=="yes"):
+            num1 = result
+        elif wantcontinue=='rs':
+            exit = True
+            calculator()
+        elif wantcontinue=='exit':
+            exit = True
+        
+calculator()
