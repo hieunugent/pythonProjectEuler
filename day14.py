deefcb88078621bba388d133aeac7ee843c105f8
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-05 02:54:59 -0800

    add day 14

--- day14.py
+++ day14.py
@@ -1,6 +1,7 @@
-from art import logoHL
+from art import logoHL, vs
 from game_data import data
 import random
+import os
 
 def resultfun(win, score):
     prompt=""
@@ -12,23 +13,44 @@ def resultfun(win, score):
         prompt = "Your are right. "    
         resultstring = " Current score "+ str(score)
     print(prompt + resultstring)
+def printinfo(data, who):
+    print("Choice "+ who +" is " + data.get("name")+" is a "+ data.get("description")+" in " + data.get("country"))
 
 
-print(logoHL)
-resultfun(True, 8)
-print(len(data))
-alreadydone =[]
-isyes = "y"
-while isyes== "y":
-    a = random.randint(0,len(data))
-    while (alreadydone.index(a) in list):
-        a = random.randint(0,len(data))
-    while (alreadydone.index(b) in list):
-        b = random.randint(0,len(data))
-        while (a == b):
-            b=random.randint(0,len(data))
-    alreadydone.append(a)
-    alreadydone.append(b)
-    print(a)
-    print(b)
-    isyes = input("is more input")
+
+# alreadydone =[]
+gameover = False
+score =0
+iswin = True
+while not gameover:
+    os.system("clear")
+    print(logoHL)
+    if (score > 0):
+        resultfun(iswin, score)
+    a = random.randint(0,len(data)-1)
+    b = random.randint(0,len(data)-1)
+    while (a == b):
+        b=random.randint(0,len(data)-1)
+    
+    printinfo(data[a], "A")
+    print(vs)
+    printinfo(data[b], "B")
+   
+    winner =""
+    if (data[a].get("follower_count") > data[b].get("follower_count")):
+            winner = "a"
+    elif (data[a].get("follower_count") < data[b].get("follower_count")):
+            winner = "b"
+    print(winner)
+    yourchoice = input("Who has higher follower A or B ? ").lower()
+    if winner == yourchoice:
+        score+=1
+    else:
+        iswin=False
+        gameover = True
+        os.system("clear")
+        print(logoHL)
+        resultfun(iswin, score)
+        
+
+        
