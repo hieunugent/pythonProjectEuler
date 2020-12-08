7e272f7a778124934e4d09d6d64e8de49dace2a1
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-07 23:41:56 -0800

    day 16

---
+++ day16/main.py
@@ -0,0 +1,13 @@
+print("Hello world")
+from turtle import Turtle, Screen
+timy = Turtle()
+timy.shape("turtle")
+timy.color("coral")
+print(timy)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+timy.forward(100)
+    
