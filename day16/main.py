379b3f0354535d69e3d48cc441457c843b8ccc50
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-08 18:18:30 -0800

    add finished day16

--- day16/main.py
+++ day16/main.py
@@ -1,13 +1,33 @@
-print("Hello world")
-from turtle import Turtle, Screen
-timy = Turtle()
-timy.shape("turtle")
-timy.color("coral")
-print(timy)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import  CoffeeMaker
+from money_machine import MoneyMachine
 
 
-timy.forward(100)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input("What would you like?"+ options + ": ")
+    if choice == "off":
+        is_on = False 
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            coffee_maker.make_coffee(drink)
+
+
+
+
+
+
     
