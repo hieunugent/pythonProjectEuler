d5621fc3643b0a5d160c145f81a02807aecb1f3c
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-09 23:41:48 -0800

    add

--- pychamenv/day17.py
+++ pychamenv/day17.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    new_question = Question(q_text=question["text"], q_answer=question["answer"])
+    new_question = Question(q_text=question["question"], q_answer=question["correct_answer"])
     question_bank.append(new_question)
 quiz = QuizBrain(question_bank)
 
