abb431b74c4d4e898f77a327417ba16b8bf5e21e
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-14 23:21:36 -0800

    add snake

---
+++ hirst-painting/day20.py
@@ -0,0 +1,17 @@
+# build snake game 
+#Create  snake 
+# Move the snake
+# control snake
+# detect collition with food
+# create a scoreboard
+# detect collition with wall
+# dectec collition with tail 
+
+from  turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+screen.exitonclick()
