8f1c0d9f0c19100925daefc37bdbb23f7bb1d5ce
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-15 20:31:46 -0800

    add score board

--- hirst-painting/day20.py
+++ hirst-painting/day20.py
@@ -10,6 +10,7 @@
 from  turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -18,6 +19,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 snake = Snake()
 food = Food()
+score = Scoreboard()
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -31,11 +33,22 @@ while game_is_on:
     time.sleep(0.1)  
     snake.move()
 
-    # detect collition with snake 
+    # detect collition with food 
     if snake.head.distance(food)< 15:
         food.refresh()
-
-
+        snake.extend()
+        score.increase_score()
+     # detect colliition with wall   
+    if snake.head.xcor() >=300 or snake.head.xcor() <=-300 or snake.head.ycor() >= 300 or snake.head.ycor() <=-300:
+        game_is_on= False
+        score.game_over()
+    # detect collition with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on= False
+            score.game_over()
 
 
 screen.exitonclick()
