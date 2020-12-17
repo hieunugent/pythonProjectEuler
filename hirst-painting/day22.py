64d70ba7d49611b5816103c35d8a2b98c08182a3
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-17 01:33:55 -0800

    add paddle.py

--- hirst-painting/day22.py
+++ hirst-painting/day22.py
@@ -10,29 +10,28 @@
 
 from turtle import Screen, Turtle
 
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PING PONG GAME")
+screen.tracer(0)
 
-paddle = Turtle("square")
-paddle.color("white")
-paddle.penup()
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.goto(350,0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-def  go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-def  go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+
+
+    
+    
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
-
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+game_is_on = True
+while game_is_on:
+    screen.update()
+    
 
 
 
