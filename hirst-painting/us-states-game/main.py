7a47fee3d5866cf58d9e2b40949219012326c818
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-25 18:32:06 -0800

    update 26

--- hirst-painting/us-states-game/main.py
+++ hirst-painting/us-states-game/main.py
@@ -1,13 +1,9 @@
 import turtle
-import os
-import csv
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-image = os.path.join(THIS_FOLDER, 'backgound.gif')
-from PIL import Image
+
 screen = turtle.Screen()
 screen.title("U.S. State Game")
-images = Image.open(image)
+# images = "backgound.gif"
 
-screen.addshape(images)
-turtle.shape(images)
+# screen.addshape(images)
+# turtle.shape(images)
 screen.exitonclick()
