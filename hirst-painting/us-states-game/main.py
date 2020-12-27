ad347d5517071313a2157ed726554590c9ec3be6
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-26 22:45:28 -0800

    add file

--- hirst-painting/us-states-game/main.py
+++ hirst-painting/us-states-game/main.py
@@ -1,9 +1,13 @@
 import turtle
-
+import os
+import gif
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+background = os.path.join(THIS_FOLDER, 'backgound.gif')
 screen = turtle.Screen()
 screen.title("U.S. State Game")
-# images = "backgound.gif"
+images = background
 
-# screen.addshape(images)
-# turtle.shape(images)
+screen.addshape(images)
+turtle.shape(images)
+answer_state = screen.textinput(title="Guess the State",prompt="What's another state's name?" )
 screen.exitonclick()
