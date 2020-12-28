3fc40f0c25b2d84ef57e78301c585d4bf60a2399
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-12-28 00:54:45 -0800

    addinput answer and the state can be

--- hirst-painting/us-states-game/main.py
+++ hirst-painting/us-states-game/main.py
@@ -1,6 +1,7 @@
 import turtle
 import os
 import gif
+import pandas
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 background = os.path.join(THIS_FOLDER, 'backgound.gif')
 screen = turtle.Screen()
@@ -9,5 +10,25 @@ screen.title("U.S. State Game")
 
 # screen.addshape(images)
 # turtle.shape(images)
+fiftystate = os.path.join(THIS_FOLDER, '50_states.csv')
+data = pandas.read_csv(fiftystate)
+all_states = data.state.to_list()
+
 answer_state = screen.textinput(title="Guess the State",prompt="What's another state's name?" )
+
+
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state==answer_state]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(answer_state)
+    
+
+
+
 screen.exitonclick()
+
+
+
