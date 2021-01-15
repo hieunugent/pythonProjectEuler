c5867bcc442d2600451e64db5bff53ee63b487f7
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-01-14 23:31:24 -0800

    add

--- hirst-painting/day26.py
+++ hirst-painting/day26.py
@@ -3,6 +3,8 @@
 #  nameofnewlist = [new_item for item in list]
 #  new_list = [n+1 for n in [1,2,3,4]]
 #  short_names =[new_item for item in list if test]
+#  dictionary comprehension new_dict = {new_key:new_value for item in list}
+
 import os
 import csv
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -15,5 +17,9 @@ with open(file2) as f2:
     file2_data = f2.readlines()
 
 result =[int(num) for num in file1_data if num in file2_data ]
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result1 ={word:len(word) for word in sentence.split()}
 
-print(result)
+
+
+print(result1)
