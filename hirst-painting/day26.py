c365b438fe747a6d8e8323d9552c4cb372e82573
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-01-14 23:31:43 -0800

    add

--- hirst-painting/day26.py
+++ hirst-painting/day26.py
@@ -17,9 +17,25 @@ with open(file2) as f2:
     file2_data = f2.readlines()
 
 result =[int(num) for num in file1_data if num in file2_data ]
+print(result)
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
 result1 ={word:len(word) for word in sentence.split()}
-
-
-
 print(result1)
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+
+
+
+print(weather_f)
