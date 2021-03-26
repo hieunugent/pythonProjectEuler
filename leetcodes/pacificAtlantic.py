7d39520c07125023a95022f634b2c8460095c131
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-03-25 18:43:00 -0700

    add

--- leetcodes/pacificAtlantic.py
+++ leetcodes/pacificAtlantic.py
@@ -11,7 +11,7 @@
 # Perform BFS from each ocean. The data structure used to keep track of cells already visited has a double purpose - it also contains every cell that can flow into that ocean.
 # Find the intersection, that is all cells that can flow into both oceans.
 class Solution:
-    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+    def pacificAtlantic(self, matrix):
         if not matrix or not matrix[0]:
             return []
         num_rows, num_cols = len(matrix), len(matrix[0])
@@ -20,15 +20,17 @@ class Solution:
         for i in range(num_rows):
             pacific_queue.append((i,0))
             atlantic_queue.append((i, num_cols-1))
+            
         for i in range(num_cols):
             pacific_queue.append((0,i))
             atlantic_queue.append((num_rows-1, i))
+            
         def bfs(queue):
             reachable= set()
             while queue:
                 (row,col) = queue.popleft()
                 reachable.add((row, col))
-                for(x,y) in [(1,0), (0,1), (-1,0), (0,-1)]:
+                for (x,y) in [(1,0), (0,1), (-1,0), (0,-1)]:
                     new_row, new_col  = row+x, col+y
                     if new_row < 0 or new_row >= num_rows or new_col < 0 or new_col >= num_cols:
                         continue
@@ -38,6 +40,7 @@ class Solution:
                         continue
                     queue.append((new_row, new_col))
             return reachable
+        
         pacific_reachable = bfs(pacific_queue)
         atlantic_reachable = bfs(atlantic_queue)
         
