5b4c09f29d4a3f54f83c9746321b5ac854c0254f
hieunugent <hieunugent@gmail.com>
2021-03-29 15:28:05 -0700

    add power func

---
+++ leetcodes/power.py
@@ -0,0 +1,14 @@
+def power(x:float, y:float)-> float:
+    result = 1.0
+    power= y
+    if y < 0:
+        power = -power
+        x = 1.0/x
+    while power:
+        if power &1:
+            result *= x
+        x = x *x
+        power = power >> 1
+    return result
+    
+print(power(3,3))
