92d110f4fa0601c99f9d1d9542788c1c0093b785
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-03-30 16:06:41 -0700

    add the delete dublicate and its variant

--- leetcodes/can_reach-end.py
+++ leetcodes/can_reach-end.py
@@ -10,3 +10,15 @@ def can_reach_end(A):
 
 A =[2,3,4,1,0,0,0,0,1,1,1,3 ]    
 print(can_reach_end(A))
+
+
+def reach_end (A):
+    f = 0
+    last_index = len(A)
+    i = 0
+    while i <= f and f < last_index:
+        f = max(f, A[i]+i)
+        i+=1
+    return f >= last_index
+A =[2,3,4,1,0,0,0,0,1,1,1,3 ]    
+print(reach_end(A))
---
+++ leetcodes/delete_dublicate.py
@@ -0,0 +1,44 @@
+def delete_duplicate(A):
+    if not A:
+        return []
+    write_idx = 1
+    for i in range (1, len(A)):
+        if A[write_idx-1] != A[i]:
+            A[write_idx] = A[i]
+            write_idx += 1
+    A = A[:write_idx]
+    return A
+    
+A= [1,2,3, 3, 4, 4, 5, 6, 6, 6, 7 ,8, 9]
+A= delete_duplicate(A)
+
+
+
+def delete_key(A, key):
+    indexkeep = 0
+    for i in range(0, len(A)):
+        if A[i] != key:
+            A[indexkeep]=A[i]
+            indexkeep+=1
+    A = A[:indexkeep]
+    return A
+print(delete_key(A, 6))
+
+# append the List with m times and these must be min(2, m )
+def append_time(A, m):
+    if not A:
+        return []
+
+    minAppear = min(2,m)
+    if minAppear ==1 :
+        return delete_duplicate(A)
+    else:
+        A = delete_duplicate(A)
+        for i in range(0, len(A)):
+            A.append(A[i])
+    return sorted(A)
+        
+            
+B= [1,2,3, 3, 4, 4, 5, 6, 6, 6, 7 ,8, 9]      
+print (append_time(B, 2))
+    
