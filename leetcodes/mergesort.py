07e3d2c9ef3b2751d5ae8e209933196e5230fdd8
hieunugent <hieunugent@gmail.com>
2021-04-05 21:58:32 -0700

    add

--- leetcodes/mergesort.py
+++ leetcodes/mergesort.py
@@ -1,2 +1,14 @@
-def mergesort(nums1, nums2):
-    pass
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+      
+    if n == 0:
+        return nums1
+    current = 0
+    for i in range(n):
+            if nums1[i] > nums2[current]:
+                nums1[i], nums2[current] = nums2[current], nums1[i]
+                if current < n-1 and nums2[current] > nums2[current+1]:
+                    current += 1
+
+    nums2.sort()
+    for i in range(n):
+        nums1[m+i], nums2[i] = nums2[i], nums1[m+i]
