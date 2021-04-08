d4ef384cc6434cdb03668ec2ef242a443840bb08
hieunugent <hieunugent@gmail.com>
2021-04-07 22:31:06 -0700

    add the checkifexits

---
+++ leetcodes/checkIfExits.py
@@ -0,0 +1,10 @@
+def checkIfExist(self, arr: List[int]) -> bool:
+    store = []
+    for num in arr:
+            if not num in store:
+                if num % 2 == 0:
+                    store.append(num//2)
+                store.append(num*2)
+            else:
+                return True
+    return False
