89137f50491e8083dcd53cf51001357211f26cf1
hieunugent <hieunugent@gmail.com>
2021-04-09 11:14:15 -0700

    add finding third max

--- leetcodes/sortArrayByParty.py
+++ leetcodes/sortArrayByParty.py
@@ -21,3 +21,16 @@ def sortArrayByParity(self, A: List[int]) -> List[int]:
         elif A[fistodd] % 2 == 0:
                 fistodd += 1
     return A
+
+def thirdMax(self, nums: List[int]) -> int:
+    if len(nums)<2:
+        return nums[0]
+    nums.sort(reverse =True)
+    count = 1
+    currentmax = 0
+    for i  in range (1, len(nums)):
+        if nums[i] != nums[i-1] and count<=3:
+            count+=1
+            if count == 3:
+                currentmax = i
+    return nums[currentmax]
