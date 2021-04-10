c4ddb532d51b4991b3bcb1a90e3ba346d8bf9dbd
hieunugent <hieunugent@gmail.com>
2021-04-10 13:11:17 -0700

    add the rotate

---
+++ leetcodes/rotate.py
@@ -0,0 +1,15 @@
+def rotate(nums, k):
+    n =  len(nums)
+    k %=n
+    start= count = 0
+    while count < n:
+        current , prev = start, nums[start]
+        while True:
+            next_idx = (current+k)%n
+            nums[next_idx] , prev= prev, nums[next_idx]
+            current = next_idx
+            count +=1
+            if start == current:
+                break
+        start+=1
+
