3e611da0ab9e6bbaedb4f91962438aef64514f60
hieunugent <hieunugent@gmail.com>
2021-04-10 17:04:50 -0700

    add the intersect

--- leetcodes/rotate.py
+++ leetcodes/rotate.py
@@ -19,3 +19,24 @@ def containsDuplicate(self, nums: List[int]) -> bool:
             if nums[i] == nums[i-1]:
                 return True
         return False
+def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashtable = defaultdict(int)
+        result =[]
+        if len(nums1) > len(nums2):
+            for num in nums1:
+                hashtable[num] +=1
+            for num in nums2:
+                if hashtable[num] > 0:
+                    result.append(num)
+                    hashtable[num] -=1
+        else:
+            for num in nums2:
+                hashtable[num] +=1
+            for num in nums1:
+                if hashtable[num] > 0:
+                    result.append(num)
+                    hashtable[num] -=1
+
+        
+        
+        return result 
