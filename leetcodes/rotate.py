07bc7441bef44a41db165aca9135d07a48da99d0
hieunugent <hieunugent@gmail.com>
2021-04-12 16:42:19 -0700

    add to the new rotate 2D

--- leetcodes/rotate.py
+++ leetcodes/rotate.py
@@ -1,4 +1,4 @@
-def rotate(nums, k):
+def rotate2(nums, k):
     n =  len(nums)
     k %=n
     start= count = 0
@@ -56,3 +56,22 @@ def plusOne(self, digits: List[int]) -> List[int]:
     if carry == 1:
         return [1] + digits
     return digits
+
+def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+        # self.transpose(matrix)
+def transpose(self, matrix):
+        n = len(matrix)
+        for i in range (n):
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+                
