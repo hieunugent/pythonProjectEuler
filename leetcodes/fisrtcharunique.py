72869983af1226ff1ab1a4f1b76ab9e44e4217b1
hieunugent <hieunugent@gmail.com>
2021-04-13 22:49:32 -0700

    add leeting

--- leetcodes/fisrtcharunique.py
+++ leetcodes/fisrtcharunique.py
@@ -1,2 +1,12 @@
-count ={'a':1, 'b':5}
-print(count['b'])
+def firstUniqChar(self, s: str) -> int:
+    setS = set(s)
+    count = {}
+    for ch in setS:
+            count[ch] = 0
+    for ch in s:
+            count[ch] = int(count[ch]) + 1
+
+    for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+    return -1
--- leetcodes/isPalindomeString.py
+++ leetcodes/isPalindomeString.py
@@ -3,3 +3,27 @@ print('a' <= 'z' and 'a'>= 'a')
 new ="a"
 new +="x"
 print(new)
+
+
+def isPalindrome(self, s: str) -> bool:
+    s = s.lower()
+
+    def removeextra(s):
+            new = ''
+            for c in s:
+                if c >= 'a' and c <= 'z' or (c >= '0' and c <= '9'):
+                    new = new + c
+            return new
+
+    def isPalindromecheck(s):
+            l = 0
+            r = len(s)-1
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+    s = removeextra(s)
+
+    return isPalindromecheck(s)
