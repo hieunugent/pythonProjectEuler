b015e834e24c35a9cc5029aa67467a547b80c51f
hieunugent <hieunugent@gmail.com>
2021-04-14 21:33:02 -0700

    update error

--- leetcodes/nextpermutation.py
+++ leetcodes/nextpermutation.py
@@ -1,15 +1,16 @@
 def next_permutation(perm):
-    k = len(perm)-2
-    for k in range (len(perm)-2, -1, -1):
-        if perm[k] <= perm[k+1]:
+    k = 0
+    for i in range (len(perm)-2, -1, -1):
+        if perm[i] < perm[i+1]:
+            k = i
             break
     if k ==-1:
         return []
-    for i in range (k+1, -1, -1):
+    for i in range (len(perm)-1, k, -1):
         if perm[i] > perm[k]:
             perm[i], perm[k] = perm[k], perm[i]
             break
     return perm 
 
-perm =[4,3,2,1,2,1]
+perm =[6,2,4,5,3,1,0]
 print(next_permutation(perm))
