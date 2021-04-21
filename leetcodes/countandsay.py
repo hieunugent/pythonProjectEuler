89ad333f9b23253e1cccd182a12582bba57a67a3
hieunugent <hieunugent@gmail.com>
2021-04-20 18:16:25 -0700

    update countandsay

--- leetcodes/countandsay.py
+++ leetcodes/countandsay.py
@@ -1,25 +1,30 @@
+
+def readletter(word):
+    if len(word) == 1:
+        return "11"
+    currentWord = word[0]
+    result = ""
+    count = 1
+    for i in range(1, len(word)):
+        if currentWord == word[i]:
+            count+=1
+        elif currentWord != word[i]:
+            result += str(count) +currentWord
+            count = 1
+            currentWord = word[i]
+        if i == len(word)-1:
+            result += str(count) + currentWord
+    return result
 def countsay(n):
     if n == 1:
-        return 1
-    step =1
-def readletter(n):
-    count = 1
-    result=""
-  
-    for i in range(1,len(n)):      
-        if n[i] != n[i-1]:
-            temp = str(count) + n[i-1]
-            result += temp
-            count = 1
-
-        elif i == len(n)-1:
-            count+=1
-            temp = str(count) + n[i]
-            result += temp
-        else:
-            count+=1
-            count = count +count
+        return "1"
+    step = 1
+    result = "1"
+    while step < n:
+        result = readletter(result)
+        step +=1
     return result
 
-print(readletter("1112224"))        
+
+print(countsay(4))  
 
