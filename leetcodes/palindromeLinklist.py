643c29756d4972fe28aa19e2f8c47f90884b402e
hieunugent <hieunugent@gmail.com>
2021-04-27 22:43:28 -0700

    add the code

--- leetcodes/palindromeLinklist.py
+++ leetcodes/palindromeLinklist.py
@@ -1,3 +1,29 @@
 def isPalindrome(head):
     temp = head
-    pass
+    length = 0
+    while temp != None:
+            length +=1
+            temp =temp.next
+    mid = length//2
+    skipmid = False
+    if length %2 != 0:
+            skipmid = True
+    count = 0
+    pointer = head
+    reverse =[]
+    while count < mid:
+            count +=1
+            current = ListNode(val =pointer.val, next = None)
+            current.next = reverse
+            reverse = current
+            pointer = pointer.next
+    if skipmid:
+            pointer = pointer.next
+    pointer = pointer.next
+       
+    while pointer!= None:
+            if pointer.val != reverse.val:
+                return False
+            pointer = pointer.nextS
+            reverse = reverse.next
+    return True
