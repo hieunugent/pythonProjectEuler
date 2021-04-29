8ce75eb638abcdd569abd2705715b95968b37c2c
hieunugent <hieunugent@gmail.com>
2021-04-29 10:24:15 -0700

    add the symmetric

---
+++ leetcodes/issymmetic.py
@@ -0,0 +1,18 @@
+def isSymmetric(self, root: TreeNode) -> bool:
+    queue = []
+    queue.append(root)
+    queue.append(root)
+    while queue:
+            t1 = queue.pop(0)
+            t2 = queue.pop(0)
+            if t1 == None and t2 == None:
+                continue
+            if t1 == None or t2 == None:
+                return False
+            if t1.val != t2.val:
+                return False
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)s
+            queue.append(t2.left)
+    return True
