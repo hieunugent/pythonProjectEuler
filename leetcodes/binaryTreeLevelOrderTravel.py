fa256899e9f9d034eab2cffe35b3a6414fe67da3
hieunugent <hieunugent@gmail.com>
2021-04-29 17:53:52 -0700

    add levelorder travel

---
+++ leetcodes/binaryTreeLevelOrderTravel.py
@@ -0,0 +1,18 @@
+def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    stack = []
+    stack.append(root)
+    result = []
+    while stack:
+            temp = []
+            tempstack = []
+            while stack:
+                tempNode = stack.pop(0)
+                if tempNode == None:
+                    continue
+                temp.append(tempNode.val)
+                tempstack.append(tempNode.left)
+                tempstack.append(tempNode.right)
+            stack = tempstack
+            if temp:
+                result.append(temp)
+    return result
