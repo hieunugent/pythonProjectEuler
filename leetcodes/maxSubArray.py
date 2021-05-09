9299cbb83495e7deea85b42a1023b1495173cf76
hieunugent <hieunugent@gmail.com>
2021-05-08 17:16:12 -0700

    add the maxsubarray

--- leetcodes/maxSubArray.py
+++ leetcodes/maxSubArray.py
@@ -1,2 +1,14 @@
-def maxSubArray(nums):
-    pass
+def maxSubArray(self, nums: List[int]) -> int:
+    length = len(nums)
+    numsleft = [0]*length
+    numsleft[0] = nums[0]
+    maxnum = nums[0]
+    for i in range(1, len(nums)):
+            temp = nums[i] + numsleft[i-1]
+            if temp > nums[i]:
+                numsleft[i] = temp
+            else:
+                numsleft[i] = nums[i]
+            if maxnum < numsleft[i]:
+                maxnum = numsleft[i]
+    return maxnum
