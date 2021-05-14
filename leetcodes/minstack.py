04b075342a3ceab57f8352831e1bd9ebe9195ff9
hieunugent <hieunugent@gmail.com>
2021-05-13 23:44:29 -0700

    add minstack

---
+++ leetcodes/minstack.py
@@ -0,0 +1,19 @@
+class MinStack:
+    
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def push(self, val: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
