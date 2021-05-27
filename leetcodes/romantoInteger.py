76900c7eeb60fda31507c5a72b0c68e37ee70f76
hieunugent <hieunugent@gmail.com>
2021-05-27 15:59:49 -0700

    add the romantointer

--- leetcodes/romantoInteger.py
+++ leetcodes/romantoInteger.py
@@ -1,2 +1,26 @@
-def romanToInt(self, s: str) -> int:
-    pass
+ def romanToInt(self, s: str) -> int:
+        value = {
+            'I':1,
+            'V':5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        total = 0
+        i = 0
+        while i < len(s):
+            s1 = value[s[i]]
+            if i+1<len(s):
+                s2 = value[s[i+1]]
+                if (s1 >= s2):
+                    total = total +s1
+                    i+=1
+                else:
+                    total = total -s1
+                    i+=1
+            else:
+                total = total + s1
+                i+=1
+        return total       
