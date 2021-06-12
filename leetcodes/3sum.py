3097c3f2f8c2eb1d2ab9c19a4f96c5c721c0bb84
hieunugent <hieunugent@gmail.com>
2021-06-11 22:38:04 -0700

    add the remove island

--- leetcodes/3sum.py
+++ leetcodes/3sum.py
@@ -1,4 +1,6 @@
 def threeSum(self, nums: List[int]) -> List[List[int]]:
      result = []
      for i in range (len(nums)):
-          currenttarget = 
+          currenttarget = 0 -nums[i]
+          existnum = {}
+          
---
+++ leetcodes/removeIsland.py
@@ -0,0 +1,24 @@
+matrix = [
+    [1,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,1,0,1],
+    [1,0,1,0,1],
+    [1,0,0,0,0]
+]
+def addborder(matrix,lenrow,lencol): 
+    border={}
+    for i in range(lencol):
+        for j in range:
+            pass
+    return border
+def removeIslands(matrix):
+    lenrow = len(matrix)
+    lencol = len(matrix[0])
+    visited = [[False]*lencol]*lenrow
+    
+
+    border = {}
+
+
+    return visited
+print(removeIslands(matrix))
