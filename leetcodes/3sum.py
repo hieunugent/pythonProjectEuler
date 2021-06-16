93e38aed4efb28cee9948a24c6997b7cdb0aaede
hieunugent <hieunugent@gmail.com>
2021-06-16 00:07:58 -0700

    add 3 sum

--- leetcodes/3sum.py
+++ leetcodes/3sum.py
@@ -1,6 +1,21 @@
 def threeSum(self, nums: List[int]) -> List[List[int]]:
-     result = []
-     for i in range (len(nums)):
-          currenttarget = 0 -nums[i]
-          existnum = {}
+     output = []
+     nums.sort()
+     for i in range(len(nums)-2):
+          left = i+1
+          right = len(nums)-1
+          while (left < right):
+               sumofthree = nums[i]+nums[left]+nums[right]
+               if sumofthree == 0:
+                    temp = [nums[i], nums[left], nums[right]]
+                    if temp not in output:
+                         output.append(temp)
+                    left += 1
+                    right -= 1
+               elif sumofthree < 0:
+                    left += 1
+               elif sumofthree > 0:
+                    right -= 1
+     return output
+
           
