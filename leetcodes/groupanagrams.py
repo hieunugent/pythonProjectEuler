e7a9ce1f701be448b57fd47f710c9f7d51eb2cf1
hieunugent <hieunugent@gmail.com>
2021-06-20 20:33:50 -0700

    add the group anagram

--- leetcodes/groupanagrams.py
+++ leetcodes/groupanagrams.py
@@ -1,2 +1,13 @@
+import collections
+
+
+
 def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    pass
+    ans = collections.defaultdict(list)
+    for s in strs:
+        ans[tuple(sorted(s))].append(s)
+    return ans.values()
+
+
+A = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(groupAnagrams(A))
