103e93aa46b09207a7d34518183a7c31c9dc9636
hieunugent <hieunugent@gmail.com>
2021-06-25 15:31:32 -0700

    add solution :

---
+++ leetcodes/longestpalindrome.py
@@ -0,0 +1,20 @@
+def longestPalindrome(self, s: str) -> str:
+       def expandaroundcenter(s, left, right):
+            L = left
+            R = right
+            while L >= 0 and R < len(s) and s[R] == s[L]:
+                L -= 1
+                R += 1
+            return R
+        if not s or len(s) < 1:
+            return ""
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            len1 = expandaroundcenter(s, i , i + 1)
+            len2 = expandaroundcenter(s, i, i )
+            lenmax = max(len1, len2)
+            if lenmax > end - start:
+                start = i - (lenmax-1)//2
+                end = i + lenmax//2
+        return s[start:end]
