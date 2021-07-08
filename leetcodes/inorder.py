c44573fc6062730ca3d18fed047e456990c0ac71
hieunugent <hieunugent@gmail.com>
2021-07-07 23:28:05 -0700

    add the optimise for inorder

--- leetcodes/inorder.py
+++ leetcodes/inorder.py
@@ -1,5 +1,5 @@
 def inorderTraversal(self, root: TreeNode) -> List[int]:
-#         left root right
+#        left root right
     def helper(root, res):
         if root is not None:
             if root.left is not None:
@@ -10,3 +10,17 @@ def inorderTraversal(self, root: TreeNode) -> List[int]:
     res = []
     helper(root, res)
     return res
+
+
+def inorderTraversal2(self, root: TreeNode) -> List[int]:
+    result = []
+    stack =[]
+    current = root
+    while current or stack:
+        while current:
+            stack.append(current)
+            current = current.left  
+        current = stack.pop()
+        result.append(current.val)
+        current= current.right
+    return result
