f9f5f2545deee2ed9103efa67297c63d2d5f50dc
hieunugent <hieunugent@gmail.com>
2021-07-21 21:31:24 -0700

    add the permutation

--- leetcodes/permutation.py
+++ leetcodes/permutation.py
@@ -1,2 +1,20 @@
-def permutation(num):
-    pass
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        def backtrack(s, result):
+            if len(s) == 0:
+                results.append(result)
+                return
+            for i in range(len(s)):
+                f = s[i]
+                left = s[0:i]
+                right = s[i+1:]
+                rest = left + right
+                backtrack(rest, result+[f])
+        result = []
+        backtrack(nums, result)
+        return results
