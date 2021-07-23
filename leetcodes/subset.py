e5fb818d0adf420c144fbe285dd5d1e596f21ade
hieunugent <hieunugent@gmail.com>
2021-07-23 13:48:34 -0700

    add 2nd solutioon

--- leetcodes/subset.py
+++ leetcodes/subset.py
@@ -5,3 +5,18 @@ class Solution:
         for num in nums:
             result += [curr + [num] for curr in result]
         return result
+    def subsets2(self, nums: List[int]) -> List[List[int]]:
+            def backtrack(first=0, curr=[]):
+            if len(curr) == k:
+                output.append(curr[:])
+                return 
+            for i in range(first, n):
+                curr.append(nums[i])
+                backtrack(i+1, curr)
+                curr.pop()
+            
+            output = []
+            n = len(nums)
+            for k in range(n+1):
+                backtrack()
+            return output
