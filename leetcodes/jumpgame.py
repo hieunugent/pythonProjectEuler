189ef74be37d6342e05d85d4a0d032be07f222df
hieunugent <hieunugent@gmail.com>
2021-08-26 18:54:37 -0700

    add the ALgorithm

---
+++ leetcodes/jumpgame.py
@@ -0,0 +1,9 @@
+# last step must be reach form the first index
+# go backward to see how many step the current index
+# need to have to go to finish line
+# if the last step return 0 no need to move
+# if the current index has value more then step it need replace by zero since the previous need only one or greater to reach the end 
+#       from their point
+#       zero stand for able to reach the end
+# if  NOT add more step to current point and the next index
+#       so the previous need more step to come to CAN REACH POINT 
