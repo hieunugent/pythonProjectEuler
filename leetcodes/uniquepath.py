124394a46c907d08f3abd1169dd9430df1e4b9e8
hieunugent <hieunugent@gmail.com>
2021-08-27 21:54:10 -0700

    add the optimize

--- leetcodes/uniquepath.py
+++ leetcodes/uniquepath.py
@@ -4,3 +4,6 @@
 # add up the top and left to found the current index value
 # until the end of array
 # return the last index of array at [m-1][n-1]
+# improve algorithm:
+# only run a haft bottom of the array
+# the diagonal will be the sum of the index [i-1][j-1] + [i][j-1] +[i][j-2]
