176655683520799cdaaaaf0bb0ada26eeb9b80aa
hieunugent <hieunugent@gmail.com>
2021-08-27 21:35:50 -0700

    solution of uniquepath

---
+++ leetcodes/uniquepath.py
@@ -0,0 +1,6 @@
+# brutal force:
+# make the m by n array start with all 1 initialize 
+# start interate the inner side start border at 1->m and 1-> n
+# add up the top and left to found the current index value
+# until the end of array
+# return the last index of array at [m-1][n-1]
