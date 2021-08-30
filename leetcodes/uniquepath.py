452f67b0aee70468ba3a0f23a22235d6fa7b7128
hieunugent <hieunugent@gmail.com>
2021-08-29 20:57:25 -0700

    add the this

--- leetcodes/uniquepath.py
+++ leetcodes/uniquepath.py
@@ -8,3 +8,5 @@
 # only run a haft bottom of the array
 # the diagonal will be the sum of the index [i-1][j-1] + [i][j-1] +[i][j-2]
 # the max number of m and n will be the row
+# the their is the column number must be equal row then calculate until diagonal 
+# 
