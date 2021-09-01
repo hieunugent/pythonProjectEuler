92f553595d0d4803962ad6eeda326aa67d10ddf8
hieunugent <hieunugent@gmail.com>
2021-08-31 23:07:17 -0700

    add the brutal force

---
+++ leetcodes/coinchange.py
@@ -0,0 +1,4 @@
+#brutal force :
+# iterater all the coin type available and the find  out the final solution 
+# and we must return the smallest step to do the change
+# but this is Excess the limit time 
