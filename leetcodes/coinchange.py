c69f4096e62688e0a92bf03a57c99ae2ef005c28
hieunugent <hieunugent@gmail.com>
2021-09-01 22:48:31 -0700

    add the solution

--- leetcodes/coinchange.py
+++ leetcodes/coinchange.py
@@ -2,3 +2,7 @@
 # iterater all the coin type available and the find  out the final solution 
 # and we must return the smallest step to do the change
 # but this is Excess the limit time 
+# using the Dynamic problem:
+# not recalculate the already done step just used it 
+#  store the step calculate it in array list 
+#  only get the min step possible 
