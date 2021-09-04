9cf17e09e442b2cfab2466baf1dc7cb135bcfcea
hieunugent <hieunugent@gmail.com>
2021-09-03 19:17:33 -0700

    add more

--- leetcodes/coinchange.py
+++ leetcodes/coinchange.py
@@ -1,8 +1,10 @@
 #brutal force :
-# iterater all the coin type available and the find  out the final solution 
+# iterated all the coin type available and the find  out the final solution 
 # and we must return the smallest step to do the change
 # but this is Excess the limit time 
 # using the Dynamic problem:
 # not recalculate the already done step just used it 
 # store the step calculate it in array list 
 # only get the min step possible 
+# 
+# 
