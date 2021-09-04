d1794eb56e1b778f8a8174b7a57b61f0c07743f5
hieunugent <hieunugent@gmail.com>
2021-09-04 14:45:42 -0700

    add the playground

--- leetcodes/playground.py
+++ leetcodes/playground.py
@@ -1,15 +1,45 @@
-print([1]+ [0]*10)
+# print([1]+ [0]*10)
 A = [1,6,3,4,5,2,7]
 B = A
-print(B)
+# print(B)
 C = list(A)
-print(C)
+# /print(C)
 E=A[:]
 
-print(A[4:0:-2])
+# print(A[4:0:-2])
 
 
 A[0], A[len(A)-1]= A[len(A)-1], A[0]
-print(A)
+
+class BST: 
+    def __init__(self, val): 
+        self.val = val
+        self.right = None
+        self.left = None
+def insert(root, key):
+    if root is None: 
+        return BST(key)
+    else: 
+        if root.val == key: 
+            return root
+        elif root.val < key: 
+            root.right= insert(root.right, key)
+        else: 
+            root.left = insert(root.left, key)
+    return root
+
+A = [100,20,500,10 , 30 ,400, 600]
+tree = BST(100)
+for a in A: 
+    tree = insert(tree, a)
+
+result = []
+curr_depth_node = [tree]
+while curr_depth_node: 
+    result.append([curr.val for curr in curr_depth_node])
+    curr_depth_node = [child for curr in curr_depth_node 
+                for child in (curr.left, curr.right) if child]
+# print(result)
+
 
 
--- leetcodes/shortestEqivalentpath.py
+++ leetcodes/shortestEqivalentpath.py
@@ -4,4 +4,20 @@ def shorted_equivalent_path(path: str)-> str:
     path_names = []
     if path[0] =='/':
         path_names.append('/')
-    for token in (token for token in path.split('/'))
+    for token in (token for token in path.split('/') 
+                    if token not in ['.','']):   
+        if token =='..' : 
+            if not path_names or path_names[-1] == '..' : 
+                path_names.append(token)
+            else: 
+                if path_names[-1] == '/': 
+                    raise ValueError('Path Error')
+                    path_names.pop()
+        else: 
+            path_names.append(token)
+    result = '/'.join(path_names)
+    return result[result.startswith('//'):]
+
+
+# path = ""
+# print(shorted_equivalent_path(path))
