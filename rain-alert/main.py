b9582149e2d9b28a6c90bd173ea98d338f9bff5b
hieunugent <hieunugent@gmail.com>
2022-01-28 00:01:28 -0800

    update

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,9 +1,13 @@
-apikey=""
+
 from http import client
 import requests
 import json
-account_sid =""
-auth_token=""
+import os 
+from twilio.rest import Client
+
+apikey = os.environ['APIKEY']
+account_sid = os.environ['ACCOUNT_SID']
+auth_token= os.environ['AUTH_TOKEN']
 parameter={
     "lat": "37.8267",
     "lon": "-122.4233",   
@@ -11,41 +15,45 @@ parameter={
 }
 
 will_rain = False
-# response = requests.get(
-#     "https://api.openweathermap.org/data/2.5/onecall", params=parameter)
-# response.raise_for_status()
-# weather_data = response.json()
-# weather_slice = weather_data["hourly"][:12]
-# for hour_data in weather_slice:
-#     condition_code = hour_data["weather"][0]["id"]
-#     if int(condition_code) < 700:
-#         will_rain = True
+response = requests.get(
+    "https://api.openweathermap.org/data/2.5/onecall", params=parameter)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
 
-# if will_rain:
+if will_rain:
+    print("It will rain")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It will rain",
+        from_="+12058360025",
+        to="+917007870983"
+    )
+ 
+
+
+
+
+# with open("rain-alert/rain-alert.json", "r") as data_file:
+#     weather_data = json.load(data_file)
+#     # print(old_data["hourly"][0]["weather"][0]["id"])
+#     weather_slice = weather_data["hourly"][0:10]   
+#     for hour_data in weather_slice:
+#         condition_code = hour_data["weather"][0]["id"]
+#         if int(condition_code) < 700:
+#             will_rain = True
+# if will_rain: 
 #     print("It will rain")
+#     client = Client(account_sid, auth_token)
+#     message = client.messages.create(
+#         body="It will rain",
+#         from_="+12058360025",
+#         to="+917007870983"
+#     )
+    
 # else:
 #     print("It will not rain")
-
-
-
-
-with open("rain-alert/rain-alert.json", "r") as data_file:
-    weather_data = json.load(data_file)
-    # print(old_data["hourly"][0]["weather"][0]["id"])
-    weather_slice = weather_data["hourly"][0:10]   
-    for hour_data in weather_slice:
-        condition_code = hour_data["weather"][0]["id"]
-        if int(condition_code) < 700:
-            will_rain = True
-# from twilio.rest import Client 
-if will_rain: 
-    print("It will rain")
-    # client = Client(account_sid, auth_token)
-    # message = client.messages.create(
-    #     body="It will rain",
-    #     from_="+12058360025",
-    #     to="+917007870983"
-    # )
-    
-else:
-    print("It will not rain")
