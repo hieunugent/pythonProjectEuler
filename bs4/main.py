a95300edac4d0ca3549b8be698d2e49805c6cba6
hieunugent <hieunugent@gmail.com>
2022-02-01 21:49:21 -0800

    update

--- bs4/main.py
+++ bs4/main.py
@@ -1,11 +1,23 @@
-from math import factorial
+
 from bs4 import BeautifulSoup
 
 
-with open("bs4/myWeb.html") as f :
+with open("bs4/myWeb.html", encoding="utf8") as f :
     contents = f.read()
     
     
-# soup = BeautifulSoup(contents, "html.parser")
+soup = BeautifulSoup(contents, "html.parser")
 
-# print(soup)
+
+anchor = soup.find_all(name ="a")
+for  tag in anchor:
+    print(tag.get("href"))
+    
+heading = soup.find(name = "h1", id="name")
+print(heading)
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.get("class"))
+name = soup.select_one(selector="#name")
+print(name)
+headings = soup.select(".heading")
+print(headings)
