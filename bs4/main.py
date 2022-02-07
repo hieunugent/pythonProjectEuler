19db2f8223bbfc020425a7c3a014216be99c70c3
hieunugent <hieunugent@gmail.com>
2022-02-06 22:25:17 -0800

    update

--- bs4/main.py
+++ bs4/main.py
@@ -6,9 +6,11 @@ yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, 'html.parser')
 article_tag = soup.find(name="a", class_="titlelink")
-print(article_tag.getText())
+article_text = article_tag.getText()
+article_link = article_tag.get('href')
+article_upvote = soup.find(name="span", class_="score").getText()
 
-
+print
 
 
 
