ef86d47a6613f40fd18728a84e7bb0adf74a3608
hieunugent <hieunugent@gmail.com>
2022-02-14 22:54:19 -0800

    upadate

--- linkinapplyBot/main.py
+++ linkinapplyBot/main.py
@@ -5,9 +5,6 @@ import os
 
 ACCOUNT_EMAIL = os.environ.get("L_NAME")
 ACCOUNT_PASSWORD = os.environ.get("L_PASS")
-
-print(ACCOUNT_EMAIL)
-print(ACCOUNT_PASSWORD)
 chrome_driver_path = "C:\Development\chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_path)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=2918703334&geoId=102095887&keywords=python%20developer&location=California%2C%20United%20States")
@@ -24,6 +21,6 @@ input_password.send_keys(ACCOUNT_PASSWORD)
 input_password.send_keys(Keys.ENTER)
 
 time.sleep(5)
-Easy_apply_button = driver.find_element_by_xpath(
-    "/html/body/div[6]/div[3]/div[3]/section/div/div/div/ul/li[8]/div/button")
-Easy_apply_button.click()
+all_listings = driver.find_element_by_css_selector(".job-search-results")
+for list in all_listings:
+    print("called")
