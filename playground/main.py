f0758b4079e413d22362691c5a52b39692710edc
hieunugent <hieunugent@gmail.com>
2022-02-19 20:41:37 -0800

    update decoration

---
+++ playground/main.py
@@ -0,0 +1,14 @@
+import time
+def decoration_function(func):
+    def wrapper_function():
+        time.sleep(1)
+        print('Wrapper executed this before' )
+        func()
+        time.sleep(1)  
+    return wrapper_function
+
+@decoration_function
+def hello_world():
+    print("hello_world")
+for i in range(5):  
+    hello_world()
