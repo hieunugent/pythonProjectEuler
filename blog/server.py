839d6409c43f2ca2239eb8ed7fa2aa49a16f58e2
hieunugent <hieunugent@gmail.com>
2022-02-24 14:10:57 -0800

    update

--- blog/server.py
+++ blog/server.py
@@ -1,11 +1,12 @@
 from urllib import response
 from flask import Flask, render_template
 import requests
+import json
 app = Flask(__name__)
 
-@app.route('/blog')
+@app.route('/')
 def blog():
-    response = requests.get('https://www.npoint.io/docs/3894d56cbf246dc4c5e2')
+    response = requests.get('https://api.npoint.io/3894d56cbf246dc4c5e2')
     all_post = response.json()
     return render_template('blog.html', posts=all_post)
 
