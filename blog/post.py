097e156b430bd6bc384162e20fbaa121d9f30eff
hieunugent <hieunugent@gmail.com>
2022-02-24 22:38:02 -0800

    update

---
+++ blog/post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, post_id, title, subtile, body):
+        self.id  = post_id 
+        self.title = title
+        self.subtitle = subtile
+        self.body = body
--- blog/server.py
+++ blog/server.py
@@ -1,14 +1,28 @@
-from urllib import response
+
 from flask import Flask, render_template
 import requests
-import json
+from post import Post
+
+
+posts = requests.get('https://api.npoint.io/3894d56cbf246dc4c5e2').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    post_objects.append(post_obj)
+
 app = Flask(__name__)
 
 @app.route('/')
-def blog():
-    response = requests.get('https://api.npoint.io/3894d56cbf246dc4c5e2')
-    all_post = response.json()
-    return render_template('blog.html', posts=all_post)
+def get_all_posts():   
+    return render_template('blog.html', posts=post_objects)
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for post in post_objects:
+        if post.id == index:
+            requested_post = post
+    return render_template('posts.html', post=requested_post)
 
 
 if __name__ == '__main__':
