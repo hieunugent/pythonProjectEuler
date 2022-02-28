58504c3192004d24d29f7f168af6159091815f6c
hieunugent <hieunugent@gmail.com>
2022-02-27 20:47:34 -0800

    update

--- upgradeCleanBlog/main.py
+++ upgradeCleanBlog/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+
+from email import message
+from flask import Flask, render_template, request
 import requests
 
 posts = requests.get('https://api.npoint.io/3894d56cbf246dc4c5e2').json()
@@ -7,12 +9,24 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html', all_posts=posts)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
-@app.route('/contact')
+
+
+@app.route('/contact', methods=["GET","POST"])
 def contact():
+    if request.method =="POST":
+        data = request.form
+        name = data['name']
+        email = data['email']
+        phone = data['phone']
+        message = data['message']
+        return "<h1>Thanks for contacting me. I will get back to you soon.</h1>"  
     return render_template('contact.html')
+    
+    
 @app.route('/post/<int:index>')
 def show_post(index):
     requested_post = None
