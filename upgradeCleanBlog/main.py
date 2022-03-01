43563da155edd2d834034109b9d33e295bac2c58
Henry Hieu Nguyen <hieunugent@gmail.com>
2022-02-28 23:54:14 -0800

    update:

--- upgradeCleanBlog/main.py
+++ upgradeCleanBlog/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+MYEMAIL=os.environ.get('TEST_EMAIL')
+PASSWORD=os.environ.get('TEST_PASSWORD')
 
 posts = requests.get('https://api.npoint.io/3894d56cbf246dc4c5e2').json()
 app = Flask(__name__)
@@ -21,8 +25,18 @@ def contact():
         print(data['email'])
         print(data['phone'])
         print(data['message'])
+        send_email(data['name'], data['email'], data['phone'], data['message'])
         return render_template('contact.html', msg_sent=True) 
     return render_template('contact.html', msg_sent=False)
+    
+def send_email(name, email, phone, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(MYEMAIL, PASSWORD)
+    format_form = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    server.sendmail(from_addr=MYEMAIL, to_addrs="henrynugent009@gmail.com", msg=format_form)
+    return "Email sent"
+    
     
     
 @app.route('/post/<int:index>')
