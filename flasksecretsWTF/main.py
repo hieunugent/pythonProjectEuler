8d70277e9d8a15c25d9d30b61fca7e7d24b5a9f2
Henry Hieu Nguyen <hieunugent@gmail.com>
2022-03-05 17:01:25 -0800

    update

--- flasksecretsWTF/main.py
+++ flasksecretsWTF/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired()])
@@ -12,6 +13,8 @@ class LoginForm(FlaskForm):
     
     
 app.secret_key="secret-key"
+
+Bootstrap(app)
 @app.route("/")
 def home():
     return render_template('index.html')
