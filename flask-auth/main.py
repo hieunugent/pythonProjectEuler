90d8860d15c44a0447aaa28abb34852ab200c7ac
hieunugent <hieunugent@gmail.com>
2022-04-02 15:21:42 -0700

    update login:

--- flask-auth/main.py
+++ flask-auth/main.py
@@ -1,3 +1,4 @@
+import email
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -19,6 +20,12 @@ class User(UserMixin, db.Model):
 #Line below only required once, when creating DB. 
 # db.create_all()
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/')
 def home():
@@ -28,9 +35,14 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method=="POST":
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
         new_user = User(
             email = request.form.get('email'),
-            password = request.form.get('password'),
+            password = hash_and_salted_password,
             name = request.form.get('name')
         )
         db.session.add(new_user)
@@ -39,23 +51,40 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods= ['GET', 'POST'])
 def login():
+    if request.method=="POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("User does not exist, please register or login again with different credentials")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash("Incorrect password, please try again")
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))    
+        
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
     return render_template("secrets.html")
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
-@app.route('/download/<path:file>')
-def download(file):
+@app.route('/download')
+@login_required
+def download():
     # depend on where the lib is install the directory will be set differently, for now path is replace for filename
     return send_from_directory(directory="../flask-auth/static/files/", path="cheat_sheet.pdf")
      
