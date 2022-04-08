cc42890ae11e4cc17244dab033e1381232673a0c
hieunugent <hieunugent@gmail.com>
2022-04-07 21:55:49 -0700

    update

--- playground/pandas.py
+++ playground/pandas.py
@@ -38,3 +38,34 @@
 # added a legend to tell apart which line is which by colour
 
 # smoothed out our time-series observations with .rolling().mean() and plotted them to better identify trends over time.
+
+
+# .agg()
+# .nuique()
+# .merge/join()
+
+# DataFrame("id":value1, "set_count": value2, "set_size": value3)
+# In this lesson we looked at how to:
+
+#     # and how to embed images with the <img> tag.
+# use HTML Markdown in Notebooks, such as section headings
+
+# combine the groupby() and count() functions to aggregate data
+
+# use the .value_counts() function
+
+# slice DataFrames using the square bracket notation e.g., df[:-2] or df[:10]
+
+# use the .agg() function to run an operation on a particular column
+
+# rename() columns of DataFrames
+
+# create a line chart with two separate axes to visualise data that have different scales.
+
+# create a scatter plot in Matplotlib
+
+# work with tables in a relational database by using primary and foreign keys
+
+# .merge() DataFrames along a particular column
+
+# create a bar chart with Matplotlib
