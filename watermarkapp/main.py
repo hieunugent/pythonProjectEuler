6a1f80f7ba2c78befbccee6fd1cd755298b01ccd
hieunugent <hieunugent@gmail.com>
2022-04-12 21:26:45 -0700

    update

--- watermarkapp/main.py
+++ watermarkapp/main.py
@@ -1,5 +1,5 @@
-from tokenize import String
-from PIL import Image, ImageTk
+
+import PIL.Image
 import PIL
 from tkinter import *
 from tkinter.ttk import *
@@ -11,14 +11,10 @@ import os
 # helper functions in the watermarkapp folder
 
 
-def open_file():
+def open_file(): 
     file_path = askopenfile(mode='r', filetypes=[('Image Files', '*.png *.jpg *.jpeg')])
-    if file_path:
-        imageBrowserurl.insert(0,file_path.name)
-       
-      
+    PIL.Image.open(file_path.name).save()   
 def upload_file():
-
     pgbar = Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
     pgbar.grid(row=3, columnspan=3, pady=20)
     for i in range(100):
@@ -26,7 +22,7 @@ def upload_file():
         window.update_idletasks()
         time.sleep(0.01)
     pgbar.destroy()
-   
+    return True
 def display_image():
     pass
 
@@ -35,12 +31,9 @@ def display_image():
 # Introduce the user to the watermark app
 window.config(padx=0, pady=0)
 canvas = Canvas(width=800, height=700, bg="grey")
-
 titleText = canvas.create_text(250, 450, text="", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, columnspan=12)
 canvas.itemconfig(titleText, text="Welcome to the Watermark App!")
-
-
 backBtn = Button(text="<Back", command=lambda: window.destroy())
 backBtn.grid(row=1, column=0)
 nextStepBtn = Button(text="Next >", command=lambda: window.destroy())
@@ -49,7 +42,6 @@ btnSelect = Button( text="Add Photo", command=lambda: open_file())
 btnSelect.grid(row=1,column=5, padx=10, pady=10)
 btnUpload = Button(text="Upload",command=lambda: upload_file())
 btnUpload.grid(row=1,column=6, padx=10, pady=10)
-
 waterMarkBtn = Button(text="Watermark Images",command=lambda: window.destroy())
 
 
