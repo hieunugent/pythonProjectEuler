d8dda08bd655e9fb3a4baba3d5499c5b6ba9378d
Henry Hieu Nguyen <hieunugent@gmail.com>
2022-06-10 23:57:30 -0700

    update

--- leetcodes/longestStringWoRepeat.py
+++ leetcodes/longestStringWoRepeat.py
@@ -1,6 +1,14 @@
-def lengthOfLongestSubstring(self, s: str) -> int:
-        longStr = 0:
-        for i in range(len(s)-1):
-            if s.charAt(i) != s.charAt(i-1):
-                longStr +=1
-            
+ def lengthOfLongestSubstring(self, s):
+        longStr = 0
+        for i in range( len(s)-1):
+            array=[]
+            array.append(s[i])
+            for j in range(i , len(s)-1):
+                if s[j] in array:
+                    if len(array) > longStr:
+                        longStr = len(array)
+                    break
+                else:
+                    array.append(s[j])
+                    
+        return longStr
