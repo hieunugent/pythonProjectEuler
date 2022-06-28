b055a64cee87673d1879abae746027ff6d01a21f
hieunugent <hieunugent@gmail.com>
2022-06-27 21:53:42 -0700

    update

--- leetcodes/PowerBall.py
+++ leetcodes/PowerBall.py
@@ -1,17 +1,20 @@
 
 
-sample_ball=[1,7,11,25,56,8,13,18,32,42,20,36,53,56,69,30,37,38,42,58,3,12,14,18,32,4,34,40,41,53,11,16,22,48,59,6,15,41,63,64,3,14,40,53,54,3,5,6,63,68,33,40,59,60,56,9,7,21,24,41,65,11,41,43,44,65,15,19,20,61,70,16,21,33,52,70,15,19,27,35,57,9,11,34,49,66,5,7,19,46,69,7,28,29,58,59,2,9,33,47,53]
-mega_ball = [14,20,16,22,4,3,11,24,8,25,22,24,13,9,10,17,15,2,10,24]
+
+
+
+sample_ball=[11,13,18,30,37,6,12,20,27,32,6,10,31,48,56,3,44,41,63,69,10,19,40,45,58,19,28,41,42,51,2,27,42,44,51,18,20,26,53,69,22,39,43,62,64,2, 10, 35, 44, 46, 14, 16, 36, 52, 60, 11, 41,  56, 57, 63, 27, 28, 51, 68, 69, 2, 39, 50,61, 66 ]
+power_ball =[16, 4, 12, 13, 25, 7, 25, 7, 25, 5, 7, 4, 16, 2, 22, 15]
+
+
 
 repeat_ball = [0]*71
 for i in sample_ball:
-    repeat_ball[i] += 1 
-repeat_mega = [0]*26
-for i in mega_ball:
+    repeat_ball[i] += 1
+repeat_mega = [0]*27
+for i in power_ball:
     repeat_mega[i] += 1
-
 print(repeat_mega)
-
 
 def topMega(repeat_mega):
     result = []
@@ -19,29 +22,29 @@ def topMega(repeat_mega):
     for i in range(len(repeat_mega)):
         if repeat_mega[i] == maxValue:
             result.append(i)
-
+     
     return result
 
+
 def top5num(arr):
-    result = [] 
-    while len(result)<5:
+    result = []
+    while len(result) < 5:
         maxValue = max(arr)
         for i in range(len(arr)):
             if arr[i] == maxValue:
                 result.append(i)
                 arr[i] = 0
-            if len(result)==5:
+            if len(result) == 5:
                 return result
-                
+
     return result
-    
-def guessingValue (arrB, arrM):
+
+
+def guessingValue(arrB, arrM):
     powerNumber = topMega(arrM)
     for i in range(len(powerNumber)): 
         print([top5num(arrB), powerNumber[i]])
-        
+
 
 guessingValue(repeat_ball, repeat_mega)
 guessingValue(repeat_ball, repeat_mega)
-
-
