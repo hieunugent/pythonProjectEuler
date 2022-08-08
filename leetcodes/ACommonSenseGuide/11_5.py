ea85a9cd16c106d4b6b7bde3937c3b658db648ec
hieunugent <hieunugent@gmail.com>
2022-08-07 23:38:25 -0700

    update

--- leetcodes/ACommonSenseGuide/11_5.py
+++ leetcodes/ACommonSenseGuide/11_5.py
@@ -3,3 +3,10 @@ def func(row,col):
         return 1
     return func(row-1, col) + func(row, col-1)
 
+def max(array):
+    if len(array)==1:
+        return array[0]
+    if array[0]> max(array[1:len(array)-1]):
+        return array[0]
+    else:
+        return max(array[1:len(array)-1])
