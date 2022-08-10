83f872c433c2a9be73ea0950fcb7368afde0426f
hieunugent <hieunugent@gmail.com>
2022-08-09 23:23:33 -0700

    update

--- leetcodes/ACommonSenseGuide/11_5.py
+++ leetcodes/ACommonSenseGuide/11_5.py
@@ -4,10 +4,14 @@ def func(row,col):
     return func(row-1, col) + func(row, col-1)
 
 def max(array):
-    if len(array)==1:
+    if len(array)== 1:
         return array[0]
-    if array[0]> max(array[1:len(array)-1]):
+    numMax = max(array[1:(len(array)-1)])
+    if array[0] > numMax:
         return array[0]
     else:
-        return max(array[1:len(array)-1])
-print(max([1,2,3,4,55]))
+        return numMax
+def unique_paths(row, col):
+    if row == 1 or col == 1:
+        return 1
+    return unique_paths(row-1, col) + unique_paths(row, col-1)
