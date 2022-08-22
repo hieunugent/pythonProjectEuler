a38b08e35bc766c9419cdb4f767ecfd9f73124b8
hieunugent <hieunugent@gmail.com>
2022-08-21 22:02:38 -0700

    update

--- leetcodes/ACommonSenseGuide/18.1.py
+++ leetcodes/ACommonSenseGuide/18.1.py
@@ -1,3 +1,11 @@
 class TriesNode:
     def __init__(self):
         self.children = {}
+    def collectAllWord(self, node=None, word="", words=[]):
+        currentNode = node or self.root 
+        for key, childNode in currentNode.children.items():
+            if key=="*":
+                words.append(word)
+            else:
+                self.collectAllWord(childNode, word+key, words)
+        return words
