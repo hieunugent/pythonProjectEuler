bc190409f7d6bcb7d7891aadebeec904aadfce11
hieunugent <hieunugent@gmail.com>
2022-09-07 16:59:18 -0700

    update

--- leetcodes/ACommonSenseGuide/18.1.py
+++ leetcodes/ACommonSenseGuide/18.1.py
@@ -1,2 +1,7 @@
 def dfs_traverse(vertex, visited_vertices={}):
-    pass 
+    visited_vertices[vertex.value] = True
+    print(vertex.value)
+    for adjacent_vertex in vertex.adjacent_vertices:
+        if visited_vertices[adjacent_vertex.value]:
+            continue
+        dfs_traverse(adjacent_vertex, visited_vertices)
