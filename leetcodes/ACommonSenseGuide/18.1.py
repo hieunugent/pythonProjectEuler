7fae1b61e4ea2c5ab047d008ac5c80b64215e033
hieunugent <hieunugent@gmail.com>
2022-09-15 16:49:39 -0700

    update

--- leetcodes/ACommonSenseGuide/18.1.py
+++ leetcodes/ACommonSenseGuide/18.1.py
@@ -1,7 +1,7 @@
 def dfs_traverse(vertex, visited_vertices={}):
     visited_vertices[vertex.value] = True
     print(vertex.value)
-    for adjacent_vertex in vertex.adjacent_vertices:
+    for adjacent_vertex in vertex.adjacent_vertices: 
         if visited_vertices[adjacent_vertex.value]:
             continue
         dfs_traverse(adjacent_vertex, visited_vertices)
@@ -17,7 +17,6 @@ def bfs_traverse(start_vertex):
                 visited_vertex[adjacent_vertex.value] = True
                 queue.append(adjacent_vertex)
      
-def dijkstra_Algo():
-    pass                
+            
         
     
