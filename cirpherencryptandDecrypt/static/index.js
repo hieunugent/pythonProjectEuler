67a7b511d5fb599bc3f3e621d262a99b377b4b2a
Henry Hieu Nguyen <hieunugent@gmail.com>
2024-04-07 00:08:45 -0700

    update

--- cirpherencryptandDecrypt/static/index.js
+++ cirpherencryptandDecrypt/static/index.js
@@ -3,14 +3,12 @@ button.addEventListener('click', function(e){
     try{
         if (e.currentTarget===this){
             const input = document.getElementById("encryptInput")
-            console.log(input.value);
          }
     }catch(error){
         console.log(error);
     }
 })
-const python = require('python');
-const pythonFuctionEncrypt = python.import('utilities/encrypt.py');
-const result = pythonFuctionEncrypt("hieu")
 
-console.log(result);
+
+
+
--- cirpherencryptandDecrypt/views.py
+++ cirpherencryptandDecrypt/views.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
     return render_template("index.html")
+@views.route("/login", methods=['GET','POST'])
+def login():
+    if request.method =='POST':
+        return  "DO LOGING STUFF"
+    else:
+        return "SHOW LOGGING Moving"
